c9896d73cd90e4e0f54d8c295a7974b9db75e24b
Jericho Tsai <jerichotsai@gmail.com>
2018-12-27 22:02:47 +0800

    chaing *.py file name, edit token

--- main.py
+++ main.py
@@ -20,10 +20,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('5/rYAKugU/kMT+Qx9IUmRNHLCqiZ4YaS8MKsbIiW0o+21qFgWB+QcGc/2cWdJIa03saiLOBBo02rbhDsZCOs/BffOqDYRmLWkpP4o8dsl5/qSs1cr0tGg6g5bhlUJvrdkrM1LVBrvJHdw0SpyWSvRAdB04t89/1O/w1cDnyilFU=
-')
-handler = WebhookHandler('b2beae22ed6d5e669b663b867bf98094
-')
+line_bot_api = LineBotApi('5/rYAKugU/kMT+Qx9IUmRNHLCqiZ4YaS8MKsbIiW0o+21qFgWB+QcGc/2cWdJIa03saiLOBBo02rbhDsZCOs/BffOqDYRmLWkpP4o8dsl5/qSs1cr0tGg6g5bhlUJvrdkrM1LVBrvJHdw0SpyWSvRAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('b2beae22ed6d5e669b663b867bf98094')
 
 
 @app.route("/callback", methods=['POST'])
